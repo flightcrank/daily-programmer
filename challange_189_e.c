ce59dc20227d2e4d8a3d42262c55204a9c3155ff
flightcrank <ocaumaxim@gmail.com>
2014-11-24 04:01:32 +1000

    added linked list

--- challange_189_e.c
+++ challange_189_e.c
@@ -1,8 +1,19 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+struct word {
+	
+	char str[256];
+	struct word *next;
+};
 
 int main () {
+
+	struct word first;
+	struct word *current = &first;
+	first.next = NULL;
 
 	//open file
 	FILE *fp = fopen("wordlist.txt", "r");
@@ -15,19 +26,43 @@ int main () {
 	}
 	
 	char str[256];
-
+	
+	//loop through all lines in the file, and put them in a string
 	while (fgets(str, 256, fp) != NULL) {
 	
-		char *nl = strchr(str, '\n');
+		//look for the new line char in the string
+		char *c = strchr(str, '\n');
 
 		//new line found
-		if (nl != NULL) {
+		if (c != NULL) {
 			
 			//change new line char into a null byte, terminating the string
-			*nl = '\0';
+			*c = '\0';
+		}
+		
+		//look for the ' char in the string
+		c = strchr(str, '\'');
+		
+		//string containing the ' char found. Skip over this word rather than deal with it
+		if (c != NULL) {
+			
+			continue;
 		}
 
-		printf("%s\n", str);
+		int len = strlen(str);
+
+		if (len >= 1 && len <= 5) {
+		
+			//copy string form file into the current linked list element
+			strcpy(current->str, str);
+
+			//allocate memory for next linked list elemnt and link the current element to it.
+			current->next = malloc(sizeof(struct word));
+
+			//change the current element to the next one for processing
+			current = current->next;
+		}
+
 	}
 
 	//close file

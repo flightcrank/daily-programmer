5220f79a20b8922c5339470b18d933d81787b938
flightcrank <ocaumaxim@gmail.com>
2014-11-28 02:49:58 +1000

    added random world selection

--- challange_189_e.c
+++ challange_189_e.c
@@ -12,6 +12,7 @@ struct word {
 //setup linked list
 struct word first;
 struct word *current = &first;
+int list_count = -1;
 
 void add_to_list(char *str) {
 			
@@ -33,11 +34,12 @@ int main () {
 	char selection[2];
 
 	do {
-
-		printf("Please Select Difficulty;\n" 
+		printf("HANGMAN !\n");
+		printf("Please Select Difficulty:\n" 
 			"1: Easy (words >= 8 chars)\n"
 			"2: Medium (words 6 - 7 chars)\n" 
-			"3: Hard (words < 5 chars)\n");
+			"3: Hard (words < 5 chars)\n"
+			"q: to quit\n");
 	
 		//get user input
 		fgets(selection, sizeof(selection), stdin);
@@ -46,14 +48,17 @@ int main () {
 
 			printf("Option %c: selected\n", selection[0]);
 
+		} else if (selection[0] == 'q') {
+			
+			puts("bye bye!");
+			return 0;
+
 		} else {
-	
+			
 			selection[0] = '0';
 		}
 
 	} while(selection[0] == '0');
-
-	
 
 	//temp string to store a single line read from the word file
 	char str[256];
@@ -96,29 +101,53 @@ int main () {
 		if (selection[0] == '1' && len >= 9) {
 			
 			add_to_list(str);
+			list_count++;
 		
 		//medium difficutly
 		} else if (selection[0] == '2' && len >= 6 && len <= 8) {
 			
 			add_to_list(str);
+			list_count++;
 		
 		//hard difficulty
 		} else if (selection[0] == '3' && len <=5){
 		
 			add_to_list(str);
+			list_count++;
 		}
 	}
 
 	//close file
 	fclose(fp);
 	
+	//select random word from list
+	int index = rand() % list_count;
+	
+	int count = 0;
 	current = &first;
+	
+	while(count <= list_count) {
+		
+		if (count == index) {
+			
+			printf("selected word = %s\n", current->str);
+		}
+			
+		current = current->next;
+		count++;
+	}
 
+	current = &first;
+	count = 0;
+	
 	while (current->next != NULL) {
 		
-		printf("%s\n", current->str);
+		//printf("%d: %s\n",count, current->str);
 		current = current->next;
+		count++;
 	}
+
+	printf("list count %d\n", list_count);
 
 	//free the linked list's memory
 	//free(...);
